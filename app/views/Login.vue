15f3ba307ad92090d1499aa5a90ee43397bf1494
jotaalvarezz <jotaalvarezz25@gmail.com>
2024-06-25 11:43:47 -0500

    prueba para mejroa en login

--- app/views/Login.vue
+++ app/views/Login.vue
@@ -25,7 +25,7 @@
           height="30%"
           width="60%"
         /> -->
-        <Label class="login-title" text="WSP"/>
+        <Label class="login-title" text="WSP" />
         <FormGroupTextField
           label="Usuario:"
           placeholder="usuario..."
@@ -48,39 +48,93 @@
           borderRadius="30"
           @tap="submit"
         />
+        <!-- ************************************** -->
+        <Button
+          backgroundColor="#F4F6F8"
+          color="#222a37"
+          text="test password"
+          style="width: 80%"
+          borderWidth="1"
+          borderColor="#222a37"
+          borderRadius="30"
+          @tap="users"
+        />
+        <!-- *************************************** -->
       </StackLayout>
     </FlexboxLayout>
   </Page>
 </template>
 
 <script>
-import { mapState, mapActions } from 'vuex';
+import { mapState, mapActions } from "vuex";
 import mixinMasters from "~/mixins/Master";
+import axios from "axios";
+const CryptoJS = require("crypto-js");
 
 export default {
   name: "LoginIndex",
   data() {
     return {
-      model:{
-        user: "",
-        password: ""
-      }
+      model: {
+        user: "adminWsp",
+        password: "Admin2024.",
+      },
+      secretKey: "security",
     };
   },
 
   mixins: [mixinMasters],
 
   methods: {
-    ...mapActions('auth',['isLogin']),
+    ...mapActions("auth", ["isLogin"]),
 
     async submit() {
       // ...
       this.loadingCharge(true);
-      const aux = await this.isLogin(this.model)
+      const aux = await this.isLogin(this.model);
       this.loadingCharge();
       this.$router.pushClear("dashboard.index");
 
       // ...
+    },
+
+    async users() {
+      try {
+        const users_wsp = await axios.get(
+          "http://172.70.9.110/mcp-backend/public/api/mobile/wsp_users"
+        );
+        const user = users_wsp.data.data[0];
+        console.log("users ", user);
+        // Separar el IV de los datos cifrados
+        // Decodificar base64
+        const decodedData = CryptoJS.enc.Base64.parse(user.wspencryptdef);
+        // Extraer IV (16 bytes) y datos cifrados
+        const iv = CryptoJS.lib.WordArray.create(
+          decodedData.words.slice(0, 4),
+          16
+        );
+        const encrypted = CryptoJS.lib.WordArray.create(
+          decodedData.words.slice(4),
+          decodedData.sigBytes - 16
+        );
+
+        console.log("ivm ", iv);
+        console.log("ecrypted ", encrypted);
+        // Decodificar los datos cifrados
+        const decryptedData = await CryptoJS.AES.decrypt(
+          encrypted,
+          CryptoJS.enc.Utf8.parse("security"),
+          {
+            iv: iv,
+            mode: CryptoJS.mode.CBC,
+            padding: CryptoJS.pad.Pkcs7,
+          }
+        ).toString(CryptoJS.enc.Utf8);
+
+        console.log("Decrypted Data: ", decryptedData);
+      } catch (error) {
+        console.log("error de usuario ", error);
+      }
     },
   },
 };
@@ -90,11 +144,10 @@ export default {
   box-shadow: 0 3px 5px 0 rgba(0, 0, 0, 0.19);
 }
 
-.img{
-
+.img {
 }
 
-.login-title{
+.login-title {
   text-align: center;
   font-size: 55px;
   font-weight: bold;
