e52b022dc0a8bd465591b51034cc130c101c215b
jotaalvarezz <jotaalvarezz25@gmail.com>
2025-02-24 13:10:30 -0500

    obtenncion de version y ajuste de vista de login

--- app/views/Login.vue
+++ app/views/Login.vue
@@ -1,46 +1,65 @@
 <template>
   <Page @loaded="verifySesion">
     <Header :search="false" />
-    <FlexboxLayout justifyContent="space-around" backgroundColor="#F4F6F8">
-      <StackLayout
-        class="img"
-        alignSelf="center"
-        backgroundColor="#F4F6F8"
-        margin="30"
-        padding="20"
-        width="510"
-        height="410"
-        borderWidth="1"
-        borderColor="#D5D8DC"
-        borderRadius="5"
-      >
-        <Label class="login-title" text="WSP" />
-        <FormGroupTextField
-          label="Usuario:"
-          placeholder="usuario..."
-          fontsize="18"
-          v-model="model.user"
-        />
-        <FormGroupTextField
-          label="Contraseña:"
-          placeholder="contraseña..."
-          fontsize="18"
-          v-model="model.password"
-          :pass="true"
-        />
-        <Stripe color="#D5D8DC" mt="20" mb="20" />
-        <Button
+    <GridLayout rows="*, auto,auto" backgroundColor="#F4F6F8" height="100%">
+      <ScrollView row="0">
+        <StackLayout
+          class="img"
+          alignSelf="center"
           backgroundColor="#F4F6F8"
-          color="#222a37"
-          text="Iniciar sesion"
-          style="width: 80%"
+          margin="30"
+          padding="20"
+          width="510"
+          height="410"
           borderWidth="1"
-          borderColor="#222a37"
-          borderRadius="30"
-          @tap="submit"
-        />
+          borderColor="#D5D8DC"
+          borderRadius="5"
+        >
+          <Label class="login-title" text="WSP" />
+          <FormGroupTextField
+            label="Usuario:"
+            placeholder="usuario..."
+            fontsize="18"
+            v-model="model.user"
+          />
+          <FormGroupTextField
+            label="Contraseña:"
+            placeholder="contraseña..."
+            fontsize="18"
+            v-model="model.password"
+            :pass="true"
+          />
+          <Stripe color="#D5D8DC" mt="20" mb="20" />
+          <Button
+            backgroundColor="#F4F6F8"
+            color="#222a37"
+            text="Iniciar sesión"
+            style="width: 80%"
+            borderWidth="1"
+            borderColor="#222a37"
+            borderRadius="30"
+            @tap="submit"
+          />
+        </StackLayout>
+      </ScrollView>
+
+      <Button
+        row="1"
+        backgroundColor="#F4F6F8"
+        color="#222a37"
+        text="update"
+        style="width: 80%"
+        borderWidth="1"
+        borderColor="#222a37"
+        borderRadius="30"
+        @tap="updateApk"
+      />
+
+      <!-- Footer fijo al final -->
+      <StackLayout row="2" backgroundColor="#D8E2E8" width="100%" padding="5">
+        <Label :text="footer" textAlignment="center" />
       </StackLayout>
-    </FlexboxLayout>
+    </GridLayout>
   </Page>
 </template>
 
@@ -48,17 +67,25 @@
 import { mapState, mapActions } from "vuex";
 import mixinMasters from "~/mixins/Master";
 import Alert from "~/alerts/Alerts";
-import localStorage from "nativescript-localstorage";
+import { getVersionName, getVersionCode } from "@nativescript/appversion";
+import {
+  getConnectionType,
+  connectionType,
+} from "@nativescript/core/connectivity";
+import { Toasty } from "@triniwiz/nativescript-toasty";
+import axios from 'axios';
+import { knownFolders, File, path, Http } from "@nativescript/core";
 
 export default {
   name: "LoginIndex",
   data() {
     return {
       model: {
-        user: "",
-        password: "",
+        user: "wadmin",
+        password: "Tecbaco2025*",
       },
-      secretKey: "security",
+      textFooter: "WSP V",
+      versionApp: "",
     };
   },
 
@@ -66,6 +93,9 @@ export default {
 
   computed: {
     ...mapState("auth", ["logout", "modules", "user"]),
+    footer() {
+      return this.textFooter + "(" + this.versionApp + ")";
+    },
   },
 
   methods: {
@@ -93,6 +123,7 @@ export default {
     },
 
     async verifySesion() {
+      this.versionApp = await getVersionName();
       try {
         this.loadingCharge(true, "Validando Usuario...");
         if (await this.isActive()) {
@@ -106,6 +137,42 @@ export default {
       } finally {
         this.loadingCharge();
       }
+    },
+
+    async download(url){
+      try {
+        const documents = knownFolders.documents()
+        const filePath = path.join(documents.path, "wsp.apk")
+        const file = File.fromPath(filePath)
+        const response = await Http.getFile(url, filePath);
+        if(response && response.path){
+          console.log("documento ",response)
+        }
+      } catch(error) {
+
+      }
+    },
+
+    async updateApk() {
+      try {
+        const connection = getConnectionType();
+        const versionApp = {};
+        versionApp.version = await getVersionName();
+        console.log("conexion ", connection, connectionType);
+        if (connection != connectionType.none) {
+          const res = await axios.post(
+            process.env.VUE_APP_API_URL + "/wsp_versions",
+            versionApp
+          );
+          console.log("res", res)
+          if (res.data.code == 1) {
+            new Toasty({ text: "Actualizacion disponible" }).show();
+            this.download(res.data.url);
+          } else {
+            new Toasty({ text: "No hay actualizacion disponible" }).show();
+          }
+        }
+      } catch (error) {}
     },
   },
 };
